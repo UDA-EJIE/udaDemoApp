6431b8919a5fc2cc23c9fc110f270d026334fee4
hmartiga <hmartinez@euro-help.es>
2018-08-29 12:58:00 +0200

    Se añade el multifilter a la tabla configurable
    Se añade el multifilter a la tabla configurable

--- x21aStatics/WebContent/3x/x21a/scripts/x21aApp/table/table.js
+++ x21aStatics/WebContent/3x/x21a/scripts/x21aApp/table/table.js
@@ -132,9 +132,9 @@ jQuery(function($){
 	var listaPlugins = 'editForm,colReorder,multiSelection,seeker,buttons,';
 	
 	var allowedPluginsBySelecionType = {
-		multiSelection: ['editForm', 'colReorder', 'seeker', 'buttons', 'groups', 'multiSelection'],
-		selection: ['editForm', 'colReorder', 'seeker', 'buttons', 'groups', 'selection'],
-		noSelection: ['colReorder', 'seeker', 'groups', 'noSelection']
+		multiSelection: ['editForm', 'colReorder', 'seeker', 'buttons', 'groups', 'multiSelection','multiFilter'],
+		selection: ['editForm', 'colReorder', 'seeker', 'buttons', 'groups', 'selection','multiFilter'],
+		noSelection: ['colReorder', 'seeker', 'groups', 'noSelection','multiFilter']
 	};
 	
 	 
@@ -261,6 +261,14 @@ jQuery(function($){
 		}else{
 			$('#groups').prop('checked', false);
 		}
+		
+		if(localStorage.plugins.indexOf('multiFilter') > -1){
+			plugins.multiFilter = { idFilter:"generated",labelSize:255,userFilter:"udaPruebas"};
+			$('#multiFilter').prop('checked', true);
+		}else{
+			$('#multiFilter').prop('checked', false);
+		}
+		
 		localStorage.clear();
 		return plugins;
 	}
