ea993749df98d27a3f2b3a89090353c5a3a8a0fa
hmartiga <hmartinez@euro-help.es>
2018-11-14 10:11:25 +0100

    Se actualizan los test
    Se actualizan los test

--- x21aAppWar/test-system/x21aAppWar/JasmineWebDriver.java
+++ x21aAppWar/test-system/x21aAppWar/JasmineWebDriver.java
@@ -1,10 +1,6 @@
 package x21aAppWar;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Properties;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.AfterClass;
@@ -26,34 +22,34 @@ public class JasmineWebDriver {
 	 //System.setProperty("webdriver.chrome.driver", "C:/bea/chromedriver.exe");
 	 driver = new ChromeDriver();
      driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-     Properties p = new Properties();
-     try {
-		p.load(new FileReader(new File("..\\x21aConfig\\x21a.properties")));
-		path = p.getProperty("statics.path");
-	} catch (FileNotFoundException e) {
-		e.printStackTrace();
-	} catch (IOException e) {
-		e.printStackTrace();
-	}
-	} 
+   
+     path = System.getProperty("otc.proyecto.url.base");
+     if(path == null || path.equals("")){
+    	 path = "https://www.ejie.ejiedes.eus/rup-cac/x21aStatics/WebContent";
+     }
+ }
 
  @Test
  public void valid_testComponentUda(){
-
+	 List<WebElement> tests = null;
 	 System.out.println("Starting test " + new Object(){}.getClass().getEnclosingMethod().getName());
      driver.get(path+"/3x/x21a/scripts/x21aApp/testJasmine/specRunner.html");	
      try{
-		 element = driver.findElement (By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='14 specs, 0 failures'])[1]"));
+		 element = driver.findElement (By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='2 specs, 0 failures'])[1]"));
+		 tests = driver.findElements(By.className("jasmine-bar jasmine-passed"));
 	 }catch (Exception e){
 		}
      Assert.assertNotNull(element);
-     System.out.println("Ending test " + new Object(){}.getClass().getEnclosingMethod().getName());
+     System.out.println("Ending test " + new Object(){}.getClass().getEnclosingMethod().getName() + " -- " +element.getText());
+    if(tests != null){
+     System.out.println("Test: " +tests.size() + " - " + tests.get(0).getText());
+    }
  }
 
 
  @AfterClass
  public static void closeBrowser(){
-	 driver.quit();
+	// driver.quit();
 	 System.out.println("Final test ");
  }
 }
