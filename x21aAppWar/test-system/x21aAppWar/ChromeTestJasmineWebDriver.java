2a2d1e0c5b3c92ea770ab935d3d312cc8a78cf6f
hmartiga <hmartinez@euro-help.es>
2018-11-20 08:35:14 +0100

    test chrome web driver
    test chrome web driver

--- x21aAppWar/test-system/x21aAppWar/ChromeTestJasmineWebDriver.java
+++ x21aAppWar/test-system/x21aAppWar/ChromeTestJasmineWebDriver.java
@@ -2,7 +2,9 @@ package x21aAppWar;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.ArrayList;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -11,15 +13,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
-public class JasmineWebDriver {
+public class ChromeTestJasmineWebDriver {
  	private static ChromeDriver driver;
- 	WebElement element;
  	static String path;
 
  @BeforeClass
  public static void openBrowser(){
-	 //System.setProperty("webdriver.chrome.driver", "C:/bea/chromedriver.exe");
 	 driver = new ChromeDriver();
      driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
    
@@ -32,24 +31,38 @@ public class JasmineWebDriver {
  @Test
  public void valid_testComponentUda(){
 	 List<WebElement> tests = null;
+	 List<Integer> results = new ArrayList<Integer>();
 	 System.out.println("Starting test " + new Object(){}.getClass().getEnclosingMethod().getName());
      driver.get(path+"/3x/x21a/scripts/x21aApp/testJasmine/specRunner.html");	
      try{
-		 element = driver.findElement (By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='2 specs, 0 failures'])[1]"));
-		 tests = driver.findElements(By.className("jasmine-bar jasmine-passed"));
+    	 tests = driver.findElements(By.className("jasmine-bar,jasmine-passed"));
 	 }catch (Exception e){
 		}
-     Assert.assertNotNull(element);
-     System.out.println("Ending test " + new Object(){}.getClass().getEnclosingMethod().getName() + " -- " +element.getText());
+     Assert.assertNotNull(tests);
+     System.out.println("Ending test " + new Object(){}.getClass().getEnclosingMethod().getName());
     if(tests != null){
-     System.out.println("Test: " +tests.size() + " - " + tests.get(0).getText());
+    	Pattern pat = Pattern.compile("[0-9]");
+        Matcher m = pat.matcher(tests.get(0).getText());
+        while (m.find()) {
+            System.out.println("  " + m.group() + " - ");
+            results.add(Integer.valueOf(m.group()));
+         }
+        
+
+        boolean testTodosCorrectos = false;
+        if(results.get(1) == 0){
+        	testTodosCorrectos = true;
+        }
+      
+        Assert.assertTrue(testTodosCorrectos);
+     System.out.println("Test: " + tests.get(0).getText());
     }
  }
 
 
  @AfterClass
  public static void closeBrowser(){
-	// driver.quit();
+	 driver.quit();
 	 System.out.println("Final test ");
  }
 }
