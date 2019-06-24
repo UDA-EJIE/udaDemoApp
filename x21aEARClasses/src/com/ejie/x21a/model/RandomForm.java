efc0b63ad1d35e8308f151a54058b55319b13576
ecarrasson <ecarrasson@euro-help.es>
2019-06-24 09:04:01 +0200

    Cambios <form:form>

--- x21aEARClasses/src/com/ejie/x21a/model/RandomForm.java
+++ x21aEARClasses/src/com/ejie/x21a/model/RandomForm.java
@@ -32,6 +32,49 @@ public class RandomForm implements java.io.Serializable{
 	
 	private Object autocompleteName2Jqueryui;
 	
+	private Object username;
+	
+	private Object password;
+	
+	private Object desde;
+	
+	private Object hasta;
+
+	private Object hora_entrada;
+
+	private Object hora_salida;
+	
+	private Object dias;
+
+	private Object cliente;
+	
+	private Object mes_enero;
+	
+	private Object mes_febrero;
+	
+	private Object mes_marzo;
+	
+	private Object mes_abril;
+	
+	private Object mes_mayo;
+	
+	private Object mes_junio;
+	
+	private Object mes_julio;
+	
+	private Object mes_agosto;
+
+	private Object mes_septiembre;
+
+	private Object mes_octubre;
+	
+	private Object mes_noviembre;
+
+	private Object mes_diciembre;
+	
+	
+	
+	
 
 	public Object getNombre() {
 		return nombre;
@@ -143,6 +186,166 @@ public class RandomForm implements java.io.Serializable{
 
 	public void setAutocompleteName2Jqueryui(Object autocompleteName2Jqueryui) {
 		this.autocompleteName2Jqueryui = autocompleteName2Jqueryui;
+	}
+
+	public Object getUsername() {
+		return username;
+	}
+
+	public void setUsername(Object username) {
+		this.username = username;
+	}
+
+	public Object getPassword() {
+		return password;
+	}
+
+	public void setPassword(Object password) {
+		this.password = password;
+	}
+
+	public Object getDesde() {
+		return desde;
+	}
+
+	public void setDesde(Object desde) {
+		this.desde = desde;
+	}
+
+	public Object getHasta() {
+		return hasta;
+	}
+
+	public void setHasta(Object hasta) {
+		this.hasta = hasta;
+	}
+
+	public Object getHora_entrada() {
+		return hora_entrada;
+	}
+
+	public void setHora_entrada(Object hora_entrada) {
+		this.hora_entrada = hora_entrada;
+	}
+
+	public Object getHora_salida() {
+		return hora_salida;
+	}
+
+	public void setHora_salida(Object hora_salida) {
+		this.hora_salida = hora_salida;
+	}
+
+	public Object getDias() {
+		return dias;
+	}
+
+	public void setDias(Object dias) {
+		this.dias = dias;
+	}
+
+	public Object getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Object cliente) {
+		this.cliente = cliente;
+	}
+
+	public Object getMes_enero() {
+		return mes_enero;
+	}
+
+	public void setMes_enero(Object mes_enero) {
+		this.mes_enero = mes_enero;
+	}
+
+	public Object getMes_febrero() {
+		return mes_febrero;
+	}
+
+	public void setMes_febrero(Object mes_febrero) {
+		this.mes_febrero = mes_febrero;
+	}
+
+	public Object getMes_marzo() {
+		return mes_marzo;
+	}
+
+	public void setMes_marzo(Object mes_marzo) {
+		this.mes_marzo = mes_marzo;
+	}
+
+	public Object getMes_abril() {
+		return mes_abril;
+	}
+
+	public void setMes_abril(Object mes_abril) {
+		this.mes_abril = mes_abril;
+	}
+
+	public Object getMes_mayo() {
+		return mes_mayo;
+	}
+
+	public void setMes_mayo(Object mes_mayo) {
+		this.mes_mayo = mes_mayo;
+	}
+
+	public Object getMes_junio() {
+		return mes_junio;
+	}
+
+	public void setMes_junio(Object mes_junio) {
+		this.mes_junio = mes_junio;
+	}
+
+	public Object getMes_julio() {
+		return mes_julio;
+	}
+
+	public void setMes_julio(Object mes_julio) {
+		this.mes_julio = mes_julio;
+	}
+
+	public Object getMes_agosto() {
+		return mes_agosto;
+	}
+
+	public void setMes_agosto(Object mes_agosto) {
+		this.mes_agosto = mes_agosto;
+	}
+
+	public Object getMes_septiembre() {
+		return mes_septiembre;
+	}
+
+	public void setMes_septiembre(Object mes_septiembre) {
+		this.mes_septiembre = mes_septiembre;
+	}
+
+	public Object getMes_octubre() {
+		return mes_octubre;
+	}
+
+	public void setMes_octubre(Object mes_octubre) {
+		this.mes_octubre = mes_octubre;
+	}
+
+	public Object getMes_noviembre() {
+		return mes_noviembre;
+	}
+
+	public void setMes_noviembre(Object mes_noviembre) {
+		this.mes_noviembre = mes_noviembre;
+	}
+
+	public Object getMes_diciembre() {
+		return mes_diciembre;
+	}
+
+	public void setMes_diciembre(Object mes_diciembre) {
+		this.mes_diciembre = mes_diciembre;
 	}
 
 }
