9b1b79ce85424fa3a6ad6f532c4d3fbee711db96
Xabi <xaabi6@users.noreply.github.com>
2022-11-11 14:37:45 +0100

    Actualizar clase de configuración
    Aplicados los cambios necesarios para volver a ser compatible con Hdiv.

--- x21aAppWar/src/com/ejie/x21a/config/UDA4HdivConfig.java
+++ x21aAppWar/src/com/ejie/x21a/config/UDA4HdivConfig.java
@@ -1,23 +1,18 @@
 package com.ejie.x21a.config;
 
-import java.util.List;
-
 import org.hdiv.config.annotation.ExclusionRegistry;
 import org.hdiv.config.annotation.RuleRegistry;
+import org.hdiv.config.annotation.ValidationConfigurer;
 import org.hdiv.config.annotation.builders.SecurityConfigBuilder;
-import org.hdiv.ee.config.annotation.ValidationConfigurer;
-import org.hdiv.ee.validator.ValidationTargetType;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.hateoas.Link;
 
+import com.ejie.x38.hdiv.config.EjieValidationConfigurer.EjieValidationConfig.EjieEditableValidationConfigurer;
 import com.ejie.x38.hdiv.config.UDA4HdivConfigurerAdapter;
-import com.hdivsecurity.services.config.EnableHdiv4ServicesSecurityConfiguration;
 
 @Configuration
 @EnableAspectJAutoProxy
-@EnableHdiv4ServicesSecurityConfiguration
 @ComponentScan("com.ejie.x38.hdiv.aspect")
 public class UDA4HdivConfig extends UDA4HdivConfigurerAdapter {
 
@@ -59,6 +54,6 @@ public class UDA4HdivConfig extends UDA4HdivConfigurerAdapter {
 
 	@Override
 	public void customConfigureEditableValidation(final ValidationConfigurer validationConfigurer) {
-		validationConfigurer.addValidation(".*/multiFilter/getDefault").forParameters("user").rules("text").target(ValidationTargetType.CLIENT_PARAMETERS);
+		((EjieEditableValidationConfigurer) validationConfigurer.addValidation(".*/multiFilter/getDefault").forParameters("user").rules("text")).setAsClientParameter(true);
 	}
 }
