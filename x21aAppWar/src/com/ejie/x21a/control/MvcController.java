87323bfa5a933ba172f0da46919902843cf8911e
Xabi <xaabi6@users.noreply.github.com>
2023-03-28 13:33:06 +0200

    Simplificar la muestra de errores

--- x21aAppWar/src/com/ejie/x21a/control/MvcController.java
+++ x21aAppWar/src/com/ejie/x21a/control/MvcController.java
@@ -15,70 +15,26 @@
 */
 package com.ejie.x21a.control;
 
-import java.io.IOException;
-
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ejie.x38.hdiv.annotation.UDALink;
-import com.ejie.x38.hdiv.annotation.UDALinkAllower;
-import com.ejie.x38.hdiv.error.ErrorResponse;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 public class MvcController {
 
 	private static final Logger logger = LoggerFactory.getLogger(MvcController.class);
-	private ObjectMapper objectMapper = new ObjectMapper();
 
-	@UDALink(name = "getErrorView", linkTo = { 
-			@UDALinkAllower(name = "getJsonErrorView")})
+	@UDALink(name = "getErrorView")
 	@RequestMapping(value = "/error")
 	public String getErrorView(Model model, HttpServletRequest request) {
 		logger.error("Ha ocurrido un error...");
-		if (isJsonRequest(request.getContentType())) {
-			// otro error
-			return "redirect:/jsonError";
-		}
 		return "error";
-	}
-
-	private boolean isJsonRequest(String contentType) {
-		MediaType requestContentType = contentType == null ? null : MediaType.valueOf(contentType);
-		return MediaType.APPLICATION_JSON.isCompatibleWith(requestContentType);
-	}
-
-	@UDALink(name = "getJsonErrorView")
-	@RequestMapping(value = "/jsonError")
-	public void getJsonErrorView(HttpServletRequest request, HttpServletResponse response) {
-		logger.error("Ha ocurrido un error...");
-
-		response.setStatus(HttpStatus.FORBIDDEN.value());
-		response.setContentType(MediaType.APPLICATION_JSON.toString());
-		response.setCharacterEncoding("UTF-8");
-
-		ErrorResponse errorResponse = (ErrorResponse) request.getAttribute("VALIDATION_ERROR");
-
-		try {
-			if (errorResponse == null) {
-				response.getWriter().write("Error desconocido");
-			} else {
-				response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
-			}
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 
 }
